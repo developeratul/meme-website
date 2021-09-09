e47a262a75a99e07832dbf508f3755e0e7bedd69
Ratul <azammmgol@gmail.com>
2021-09-09 17:00:41 +0600

    still trying

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use("/get_profile", profileRouter);
 
 // for production
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
